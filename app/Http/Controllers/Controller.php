bdd30260229c52c7e6e2b03f2c552c1f3b94f064
Gnosis <emmagab38@gmail.com>
2024-10-03 22:04:01 +0100

    commit changes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Trade;
+use Carbon\Carbon;
+
 abstract class Controller
 {
-    //
+    public function checkTradeDuration()
+    {
+        $openTrades = Trade::where('status', 'open')->get();
+
+        foreach ($openTrades as $trade) {
+            $tradeOpenedAt = Carbon::parse($trade->created_at);
+            $tradeDuration = $trade->duration;
+
+            if (now()->diffInMinutes($tradeOpenedAt) >= $tradeDuration) {
+                $trade->status = 'closed';
+                $trade->save();
+            }
+        }
+
+        // You can return or log something if needed, or leave this function as void
+    }
 }
--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+
+
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
+    $this->call('App\Http\Controllers\checkTradeDuration@checkTradeDuration')->everyMinute();
+
 })->purpose('Display an inspiring quote')->hourly();
